7edab4f4c15ff93d3ecd665f215304bef8e76655
aitortarazaga <baskonistaat@gmail.com>
2017-06-19 20:15:53 +0200

    Updated to 3on3 new system

--- World of Tanks/cup_map_randomizer.user.js
+++ World of Tanks/cup_map_randomizer.user.js
@@ -1,24 +1,28 @@
 // ==UserScript==
 // @name        esl-wot-admin-leaguewizard-7-script
 // @namespace   ESL.Wasteful
-// @description Sets all best_of and maps when building bracket in 5on5 and 7on7 cups with BAPI
+// @description Sets all best_of and maps when building bracket in 3on3, 5on5 and 7on7 cups with BAPI
 // @include     *://play.eslgaming.com/worldoftanks/*/admin_leaguewizard/7/
 // @version     1.23
 // @grant       none
 // ==/UserScript==
 /**
  * Created by Wasteful.
- * Last update: 07.12.2015. (trigger only on 7on7 and 5on5 cups)
+ * Last update: 19.06.2017. (Added 3on3 function by _Aitor_)
  * Config: current setup is go4wot, edit bestOf_* variables if you have different cup setup
  */
 
+var is3on3 = window.location.href.indexOf("3on3") > 0;
 var is7on7or5on5 = window.location.href.indexOf("test-cup-script") > 0 || window.location.href.indexOf("go4wot") > 0 || window.location.href.indexOf("7on7") > 0 || window.location.href.indexOf("5on5") > 0;
 
-if(is7on7or5on5){
-    var bestOf_BeforeFinals = 3, bestOf_finals = 5; //Edit these if needed
-    //do not edit below!!
+var maps;
 
-    var maps = [
+var bestOf_BeforeFinals = 3, bestOf_finals = 5;
+
+
+if(is7on7or5on5){
+    
+    maps = [
         "Himmelsdorf (Standard Battle; Encounter; Attack/Defense)",
         "Murovanka (Standard Battle; Encounter; Attack/Defense)",
         "Cliff (Standard Battle; Attack/Defense)",
@@ -28,6 +32,27 @@ if(is7on7or5on5){
         "Steppes (Standard Battle; Encounter; Attack/Defense)",
         "Ghost Town (Standard Battle; Attack/Defense)"
     ];
+    
+}
+else{
+    if(is3on3){
+        maps = [
+            "Himmelsdorf (Standard Battle; Encounter; Attack/Defense)",
+            "Murovanka (Standard Battle; Encounter; Attack/Defense)",
+            "Prokhorovka (Standard Battle; Encounter; Attack/Defense)",
+            "Mines (Standard Battle; Encounter; Attack/Defense)",
+            "Ruinberg (Standard Battle; Encounter; Attack/Defense)",
+            "Steppes (Standard Battle; Encounter; Attack/Defense)",
+            "Sand River (Standard Battle; Encounter; Assault; Attack/Defense)",
+            "Lakeville (Standard Battle; Encounter; Attack/Defense)",
+            "Ensk (Standard Battle; Encounter)"
+    ];
+    }
+}
+
+    //do not edit below!!
+
+    
 
     var currentBestOf;
     function shuffle(array) {
@@ -84,4 +109,3 @@ if(is7on7or5on5){
             }
         }
     });
-}
